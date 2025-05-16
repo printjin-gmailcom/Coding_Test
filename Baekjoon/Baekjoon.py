463176cc7a1213dfd5caafb94ff38ef14cfab695
printjin <printjin@gmail.com>
2025-05-16 09:16:44 +0900

    Feat : Greedy

--- Baekjoon/Baekjoon.py
+++ Baekjoon/Baekjoon.py
@@ -5038,3 +5038,15 @@ while True:
     else:
         print(time)
         break
+
+
+n = int(input())
+distances = list(map(int, input().split()))
+prices = list(map(int, input().split()))
+total_cost = 0
+min_price = prices[0]
+for i in range(n - 1):
+    if prices[i] < min_price:
+        min_price = prices[i]
+    total_cost += min_price * distances[i]
+print(total_cost)
