2aa1afb920347eda8ce9c4ac54c013f983b9547b
printjin <printjin@gmail.com>
2025-10-05 11:19:56 +0900

    Feat : Programmers 2Q

--- Programmers/KAKAO.py
+++ Programmers/KAKAO.py
@@ -879,3 +879,61 @@ def solution(n, t, m, timetable):
             else: 
                 return f"{(last - 1) // 60:02d}:{(last - 1) % 60:02d}"
         bus_time += t
+
+
+def rotate(matrix):
+    return list(zip(*matrix[::-1]))
+
+def check(new_lock):
+    n = len(new_lock) // 3
+    for i in range(n, n * 2):
+        for j in range(n, n * 2):
+            if new_lock[i][j] != 1:
+                return False
+    return True
+
+def solution(key, lock):
+    n = len(lock)
+    m = len(key)
+    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
+    for i in range(n):
+        for j in range(n):
+            new_lock[i + n][j + n] = lock[i][j]
+    for rotation in range(4):
+        key = rotate(key)
+        for x in range(n * 2):
+            for y in range(n * 2):
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] += key[i][j]
+                if check(new_lock):
+                    return True
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] -= key[i][j]
+    return False
+
+
+def solution(board, skill):
+    n, m = len(board), len(board[0])
+    diff = [[0] * (m + 1) for _ in range(n + 1)]
+    for type_, r1, c1, r2, c2, degree in skill:
+        if type_ == 1:
+            degree = -degree
+        diff[r1][c1] += degree
+        diff[r1][c2 + 1] -= degree
+        diff[r2 + 1][c1] -= degree
+        diff[r2 + 1][c2 + 1] += degree
+    for i in range(n):
+        for j in range(1, m):
+            diff[i][j] += diff[i][j - 1]
+    for j in range(m):
+        for i in range(1, n):
+            diff[i][j] += diff[i - 1][j]
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] + diff[i][j] > 0:
+                cnt += 1
+    return cnt
+
