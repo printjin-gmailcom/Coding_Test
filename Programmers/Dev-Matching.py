2b4ad5a16c4c6dc23268741a9eb840fb4dae0256
Yeonjin <161997875+printjin-gmailcom@users.noreply.github.com>
2025-11-10 09:50:38 +0900

    Feat : Programmers 1Q

--- Programmers/Dev-Matching.py
+++ Programmers/Dev-Matching.py
@@ -6,3 +6,24 @@ def solution(lottos, win_nums):
     return [highest_rank, lowest_rank]
 
 
+def solution(rows, columns, queries):
+    matrix = [[i * columns + j + 1 for j in range(columns)] for i in range(rows)]
+    answer = []
+    for x1, y1, x2, y2 in queries:
+        x1, y1, x2, y2 = x1 - 1, y1 - 1, x2 - 1, y2 - 1
+        prev = matrix[x1][y1]
+        min_value = prev
+        for y in range(y1 + 1, y2 + 1):
+            matrix[x1][y], prev = prev, matrix[x1][y]
+            min_value = min(min_value, prev)
+        for x in range(x1 + 1, x2 + 1):
+            matrix[x][y2], prev = prev, matrix[x][y2]
+            min_value = min(min_value, prev)
+        for y in range(y2 - 1, y1 - 1, -1):
+            matrix[x2][y], prev = prev, matrix[x2][y]
+            min_value = min(min_value, prev)
+        for x in range(x2 - 1, x1 - 1, -1):
+            matrix[x][y1], prev = prev, matrix[x][y1]
+            min_value = min(min_value, prev)
+        answer.append(min_value)
+    return answer
