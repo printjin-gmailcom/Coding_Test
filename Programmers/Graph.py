8d8578f98cbc46a4e3bba71635a59b25d1722a3a
Yeonjin <161997875+printjin-gmailcom@users.noreply.github.com>
2025-12-03 08:06:38 +0900

    Feat : Programmers 1Q

--- Programmers/Graph.py
+++ Programmers/Graph.py
@@ -36,3 +36,25 @@ def solution(n, vertex):
                 queue.append(neighbor)
     max_distance = max(visited)
     return visited.count(max_distance)
+
+
+def solution(arrows):
+    dx = [0,1,1,1,0,-1,-1,-1]
+    dy = [1,1,0,-1,-1,-1,0,1]
+    visited = set()
+    edges = set()
+    x, y = 0, 0
+    visited.add((x, y))
+    rooms = 0
+    for d in arrows:
+        for _ in range(2):
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if (nx, ny) in visited:
+                if ((x, y), (nx, ny)) not in edges:
+                    rooms += 1
+            visited.add((nx, ny))
+            edges.add(((x, y), (nx, ny)))
+            edges.add(((nx, ny), (x, y)))
+            x, y = nx, ny
+    return rooms
