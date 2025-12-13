4941fba9370dee63023308e0702a4b274e212916
Yeonjin <161997875+printjin-gmailcom@users.noreply.github.com>
2025-12-13 19:25:53 +0900

    Feat : programmers 1Q

--- Programmers/Binary.py
+++ Programmers/Binary.py
@@ -10,3 +10,25 @@ def solution(n, times):
         else:
             left = mid + 1  
     return answer
+
+
+def solution(distance, rocks, n):
+    rocks.sort()
+    rocks.append(distance)
+    left, right = 1, distance
+    answer = 0
+    while left <= right:
+        mid = (left + right) // 2
+        removed = 0
+        prev = 0
+        for rock in rocks:
+            if rock - prev < mid:
+                removed += 1
+            else:
+                prev = rock
+        if removed <= n:
+            answer = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    return answer
