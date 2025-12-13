6deeb6fc98a941038705d046c668498dee7b33d8
Yeonjin <161997875+printjin-gmailcom@users.noreply.github.com>
2025-12-14 00:03:33 +0900

    Feat : Programmers 1Q

--- Programmers/Dynamic.py
+++ Programmers/Dynamic.py
@@ -59,3 +59,25 @@ def solution(money):
     for i in range(2, n):
         dp2[i] = max(dp2[i-1], dp2[i-2] + money[i])
     return max(dp1[-2], dp2[-1])
+
+
+def solution(arr):
+    n = (len(arr) + 1) // 2
+    nums = [int(arr[i * 2]) for i in range(n)]
+    ops = [arr[i * 2 + 1] for i in range(n - 1)]
+    dp_max = [[-10**18] * n for _ in range(n)]
+    dp_min = [[10**18] * n for _ in range(n)]
+    for i in range(n):
+        dp_max[i][i] = nums[i]
+        dp_min[i][i] = nums[i]
+    for length in range(2, n + 1):
+        for i in range(n - length + 1):
+            j = i + length - 1
+            for k in range(i, j):
+                if ops[k] == '+':
+                    dp_max[i][j] = max(dp_max[i][j], dp_max[i][k] + dp_max[k + 1][j])
+                    dp_min[i][j] = min(dp_min[i][j], dp_min[i][k] + dp_min[k + 1][j])
+                else:
+                    dp_max[i][j] = max(dp_max[i][j], dp_max[i][k] - dp_min[k + 1][j])
+                    dp_min[i][j] = min(dp_min[i][j], dp_min[i][k] - dp_max[k + 1][j])
+    return dp_max[0][n - 1]
