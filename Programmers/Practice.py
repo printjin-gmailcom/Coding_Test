da072d081253fed9978b0c8c83559f0efbcf152b
Yeonjin <161997875+printjin-gmailcom@users.noreply.github.com>
2025-12-20 11:55:48 +0900

    Feat : Programmers 1Q

--- Programmers/Practice.py
+++ Programmers/Practice.py
@@ -1554,3 +1554,20 @@ def solution(clockHands):
         if all(board[n-1][j] == 0 for j in range(n)):
             answer = min(answer, cnt)
     return answer
+
+
+def solution(n):
+    MOD = 1000000007
+    if n == 0:
+        return 1
+    dp = [0] * (max(n + 1, 7))
+    dp[0] = 1
+    dp[1] = 1
+    dp[2] = 3
+    dp[3] = 10
+    dp[4] = 23
+    dp[5] = 62
+    dp[6] = 170
+    for i in range(7, n + 1):
+        dp[i] = (dp[i - 1] + 2 * dp[i - 2] + 6 * dp[i - 3] + dp[i - 4] - dp[i - 6]) % MOD
+    return dp[n] % MOD
