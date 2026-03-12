dc1a48432cdd5d695da76f4071cafec28a764540
Yeonjin <161997875+printjin-gmailcom@users.noreply.github.com>
2026-03-13 08:16:42 +0900

    Update LeetCode_60_problems.py

--- Leetcode/LeetCode_60_problems.py
+++ Leetcode/LeetCode_60_problems.py
@@ -125,3 +125,20 @@ class Solution:
             if len(heap) > k:
                 heapq.heappop(heap)
         return [num for freq, num in heap]
+
+
+import heapq
+class Solution:
+    def kSmallestPairs(self, nums1, nums2, k):
+        if not nums1 or not nums2:
+            return []
+        heap = []
+        res = []
+        for i in range(min(k, len(nums1))):
+            heapq.heappush(heap, (nums1[i] + nums2[0], i, 0))
+        while heap and len(res) < k:
+            s, i, j = heapq.heappop(heap)
+            res.append([nums1[i], nums2[j]])
+            if j + 1 < len(nums2):
+                heapq.heappush(heap, (nums1[i] + nums2[j+1], i, j+1))
+        return res
