c0803a2309eba7c96edf611db56b1f5e066f63c6
Yeonjin <161997875+printjin-gmailcom@users.noreply.github.com>
2026-03-17 08:47:57 +0900

    Update LeetCode_60_problems.py

--- Leetcode/LeetCode_60_problems.py
+++ Leetcode/LeetCode_60_problems.py
@@ -203,3 +203,46 @@ class Solution:
             if count[c] == 1:
                 return i
         return -1
+
+
+class Solution:
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+        m, n = len(grid), len(grid[0])
+        count = 0
+        def dfs(r, c):
+            if r < 0 or r >= m or c < 0 or c >= n or grid[r][c] == "0":
+                return
+            grid[r][c] = "0"
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    count += 1
+                    dfs(i, j)
+        return count
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid):
+        m, n = len(grid), len(grid[0])
+        def dfs(r, c):
+            if r < 0 or r >= m or c < 0 or c >= n or grid[r][c] == 0:
+                return 0
+            grid[r][c] = 0
+            area = 1
+            area += dfs(r+1, c)
+            area += dfs(r-1, c)
+            area += dfs(r, c+1)
+            area += dfs(r, c-1)
+            return area
+        max_area = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    max_area = max(max_area, dfs(i, j))
+        return max_area
