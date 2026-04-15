a5f77efd6e8578a6773666c3eef0b3515a228ae1
Yeonjin <161997875+printjin-gmailcom@users.noreply.github.com>
2026-04-16 08:14:33 +0900

    Update Baekjoon.py

--- Baekjoon/Baekjoon.py
+++ Baekjoon/Baekjoon.py
@@ -5304,3 +5304,16 @@ while l < r:
     else:
         l += 1
 print(*ans)
+
+
+import sys
+input = sys.stdin.readline
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+area = 0
+for i in range(n):
+    x1, y1 = points[i]
+    x2, y2 = points[(i + 1) % n]
+    area += x1 * y2 - x2 * y1
+area = abs(area) / 2
+print(f"{area:.1f}")
