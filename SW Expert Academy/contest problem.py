2f9594934fea885479fb58a30a4c81aee66192b7
Yeonjin <161997875+printjin-gmailcom@users.noreply.github.com>
2026-07-19 08:02:13 +0900

    Update contest problem.py

--- SW Expert Academy/contest problem.py
+++ SW Expert Academy/contest problem.py
@@ -16,3 +16,46 @@ for test_case in range(1, T + 1):
         total += R - L + 1
     print(f"#{test_case} {total}")
   
+
+T = int(input())
+for tc in range(1, T + 1):
+    n = int(input())
+    s = set()
+    for _ in range(n):
+        x = int(input())
+        if x in s:
+            s.remove(x)
+        else:
+            s.add(x)
+    print(f"#{tc} {len(s)}")
+
+
+from itertools import permutations
+
+INF = 10 ** 18
+
+T = int(input())
+for tc in range(1, T + 1):
+    N, M, R = map(int, input().split())
+    towns = list(map(int, input().split()))
+    dist = [[INF] * (N + 1) for _ in range(N + 1)]
+    for i in range(1, N + 1):
+        dist[i][i] = 0
+    for _ in range(M):
+        a, b, c = map(int, input().split())
+        if c < dist[a][b]:
+            dist[a][b] = c
+            dist[b][a] = c
+    for k in range(1, N + 1):
+        for i in range(1, N + 1):
+            for j in range(1, N + 1):
+                if dist[i][j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+    ans = INF
+    for order in permutations(towns):
+        total = 0
+        for i in range(R - 1):
+            total += dist[order[i]][order[i + 1]]
+        ans = min(ans, total)
+    print(f"#{tc} {ans}")
+
