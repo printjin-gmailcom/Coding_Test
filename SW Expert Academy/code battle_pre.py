f0ab668e8f523f66b7dbe5e73bf63ca14f512166
Yeonjin <161997875+printjin-gmailcom@users.noreply.github.com>
2026-07-22 09:40:48 +0900

    Update code battle_pre.py

--- SW Expert Academy/code battle_pre.py
+++ SW Expert Academy/code battle_pre.py
@@ -119,3 +119,74 @@ def solve():
         out.append(" ".join(ans))
     print("\n".join(out))
 
+
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+TC = int(input())
+ans = []
+for _ in range(TC):
+    N, S = input().split()
+    cnt = defaultdict(int)
+    cnt[(0, 0)] = 1
+    a = 0
+    c = 0
+    res = 0
+    for ch in S:
+        if ch == 'A':
+            a += 1
+        elif ch == 'T':
+            a -= 1
+        elif ch == 'C':
+            c += 1
+        else:  # G
+            c -= 1
+        key = (a, c)
+        res += cnt[key]
+        cnt[key] += 1
+    ans.append(str(res))
+print("\n".join(ans))
+
+
+import sys
+sys.setrecursionlimit(1 << 25)
+input = sys.stdin.readline
+TC = int(input())
+for _ in range(TC):
+    N, M, K = map(int, input().split())
+    color = [0] + list(map(int, input().split()))
+    graph = [[] for _ in range(N + 1)]
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    ans = 0
+    def dfs(now, depth, used_color, visited):
+        nonlocal_ans[0] += 1
+        if depth == K - 1:
+            return
+        for nxt in graph[now]:
+            if nxt in visited:
+                continue
+            c = color[nxt]
+            if used_color & (1 << c):
+                continue
+            visited.add(nxt)
+            dfs(
+                nxt,
+                depth + 1,
+                used_color | (1 << c),
+                visited
+            )
+            visited.remove(nxt)
+    for start in range(1, N + 1):
+        nonlocal_ans = [0]
+        dfs(
+            start,
+            1,
+            1 << color[start],
+            {start}
+        )
+        ans += nonlocal_ans[0] - 1
+    print(ans)
+    
